84ee7ea114e718a8f82d04f3b1a07886889f2f59
Rishit Roy <hellorishit.roy@gmail.com>
2026-09-23 12:43:29 +0530

    Time: 0 ms (100%), Space: 46.6 MB (43.09%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    public int[] helper(TreeNode root){
+        if(root==null){
+            return new int[]{0,0};
+        }
+
+        int[] left=helper(root.left);
+        int[] right=helper(root.right);
+
+        int rob = root.val+left[1]+right[1];
+        int not_rob=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+
+        return new int[]{rob,not_rob};
+    }
+    public int rob(TreeNode root) {
+        int[] ans=helper(root);
+        return Math.max(ans[0],ans[1]);
+        
+    }
+}
