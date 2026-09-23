8cd851a5233f856e3e9b35baf736bd277f974fca
Rishit Roy <hellorishit.roy@gmail.com>
2026-09-23 16:46:27 +0530

    Time: 17 ms (70.31%), Space: 64.1 MB (5.5%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean helper(int idx1, int idx2, String s, String p, Boolean[][] dp) {
 
-        if (idx1 < 0 && idx2 < 0) return true;
-
-        if (idx1 >= 0 && idx2 < 0) return false;
-
-        if (idx1 < 0 && idx2 >= 0) {
+        if (idx1 < 0 && idx2 < 0) return true;  // dono string consumed
+        if (idx1 >= 0 && idx2 < 0) return false;  // string2 consumed but string1 consumed nhi hua to koi chance nhi hai match ka
+        // because benefits to string2 me the isliye wo khatam to bat khatam
+        if (idx1 < 0 && idx2 >= 0) {  // string 1 khatam but string 2 hai to
+        // agar mujhe match chahiye to bak
             for (int k = 0; k <= idx2; k++) {
-                if (p.charAt(k) != '*') {
+                if (p.charAt(k) != '*' || p.charAt(k)=='?') {
                     return false;
                 }
             }
@@ -38,9 +38,7 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int m = s.length();
         int n = p.length();
-
         Boolean[][] dp = new Boolean[m][n];
-
         return helper(m - 1, n - 1, s, p, dp);
     }
 }
