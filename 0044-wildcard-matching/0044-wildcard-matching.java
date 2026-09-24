3f32a6a3151d0075991aa6495378cb0f923bd40b
Rishit Roy <hellorishit.roy@gmail.com>
2026-09-24 14:42:37 +0530

    Time: 18 ms (66.73%), Space: 64.3 MB (5.46%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -5,7 +5,7 @@ class Solution {
         if (idx1 >= 0 && idx2 < 0) return false;  // string2 consumed but string1 consumed nhi hua to koi chance nhi hai match ka
         // because benefits to string2 me the isliye wo khatam to bat khatam
         if (idx1 < 0 && idx2 >= 0) {  // string 1 khatam but string 2 hai to
-        // agar mujhe match chahiye to bak
+        // agar mujhe match chahiye to baki sab char string2 me ya * ya ? hone chahiye
             for (int k = 0; k <= idx2; k++) {
                 if (p.charAt(k) != '*' || p.charAt(k)=='?') {
                     return false;
@@ -15,12 +15,14 @@ class Solution {
         }
 
         if (dp[idx1][idx2] != null) return dp[idx1][idx2];
-
+        // dono match ya string2 is '?' so indirectly match
         if (s.charAt(idx1) == p.charAt(idx2) || p.charAt(idx2) == '?') {
             return dp[idx1][idx2] =
                     helper(idx1 - 1, idx2 - 1, s, p, dp);
         }
-
+        // if * hai to 2 cases ekbar replace with empty string(i,j-1)
+        // or replace it with actual ith char so (i-1,j)
+        
         else if (p.charAt(idx2) == '*') {
 
             boolean case1 = helper(idx1 - 1, idx2, s, p, dp);
@@ -30,7 +32,7 @@ class Solution {
             return dp[idx1][idx2] = case1 || case2;
         }
 
-        else {
+        else { // no match
             return dp[idx1][idx2] = false;
         }
     }
